bebf1154f9002332f2984280b0da18e1127ef73b
iBreakEverything <1362764+iBreakEverything@users.noreply.github.com>
2025-05-12 00:45:40 +0300

    Create build.py
    Build script

---
+++ cicd/build.py
@@ -0,0 +1,34 @@
+import os
+import re
+
+SCRIPT_FILE = 'cloudflare-template-ipv4.sh'
+
+PREFIX = 'cloudflare'
+SUFFIX = 'ipv4.sh'
+
+modes = ['crontab', 'user']
+shells = ['sh', 'bash']
+
+def main():
+  with open(SCRIPT_FILE, 'r', encoding='utf8') as f:
+    script = f.read()
+
+  for mode in modes:
+    for shell in shells:
+      # Make filename
+      filename = '-'.join([PREFIX, mode, shell, SUFFIX])
+      # Set shell
+      content = script.replace('#SHELL', f'#!/bin/{shell}')
+      
+      if mode == 'crontab': 
+        # Remove debug lines
+        content = '\n'.join([x for x in content.split('\n') if not x.endswith('#DEBUG')])
+      elif mode == 'user':
+        # Remove debug comments, convert logger to echo
+        content = content.replace(' #DEBUG', '')
+        content = content.replace('logger ', 'echo ')
+
+      with open(filename, 'w', encoding='utf8') as f:
+        f.write(content)
+
+main()
